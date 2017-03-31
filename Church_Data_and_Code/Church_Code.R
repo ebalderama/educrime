963d6a84bbd125941a822a637bd3b086c75809ea
ebalderama <ebalderama@luc.edu>
2017-03-31 11:11:27 -0500

    fixed code, added violent crimes 2016 spss data

--- Church_Data_and_Code/Church_Code.R
+++ Church_Data_and_Code/Church_Code.R
@@ -14,47 +14,69 @@ library(dplyr)
 library(broom)
 library(geosphere)
 
-#ZIP codes for a shapefile
-comm_areas<-readOGR("C:/Users/Nick Fox/Downloads/Boundaries - Community Areas/geo_export_4fbea0cb-8eca-47d2-8dd9-ba6ccb5ed7cd.shp")
-comm_areas$area_numbe<-as.character(comm_areas$area_numbe)
-comm_areas$community<-as.character(comm_areas$community)
-area_comm<-data.frame(community=comm_areas$community,id=comm_areas$area_numbe)
-area_comm$community<-as.character(area_comm$community)
-area_comm$id<-as.character(area_comm$id)
-rownames(w)
 
+
+#===========================================
+#Population data
+#===========================================
+pop.dat<-read.csv("C:/Users/Nick Fox/Downloads/2010_Census.csv", skip=1)
+pop.dat <- read.csv("Church_Data_and_Code/2010_Census.csv", skip=1)
+
+#pop.final <- data.frame(id=as.character(pop.dat$GeogKey), population=pop.dat$Total.Population)
+#pop.final$community<-toupper(as.character(pop.final$community))
+#pop.final$community[32]<-c("LOOP")
+#pop.final$community[76]<-c("OHARE")
+#pop.final$id <- as.character(1:77)
+
+
+
+#===========================================
 #Homicide crimes
-homicides<-read.csv("~/educrime/Chicago Data/homicides.csv")
-homicides$id<-as.character(homicides$Community.Area)
-a<-homicides[homicides$id=="25",]
+#===========================================
+homicides <- read.csv("Chicago Data/homicides.csv")
+
+total.homicides <- rep(0,77)
+temp1 <- table(homicides$Community.Area)
+total.homicides[as.numeric(names(temp1))] <- temp1
+
 
 #Total crime data
-crime<-read.csv("C:/Users/Nick Fox/Downloads/Crimes_-_2015.csv")
+crime <- read.csv("C:/Users/Nick Fox/Downloads/Crimes_-_2015.csv")
 crime$Primary.Type<-as.character(crime$Primary.Type)
 vio.crime<-crime[crime$Primary.Type==c("ASSAULT","BATTERY","ROBBERY","CRIM SEXUAL ASSAULT","HOMICIDE"),]
 vio.crime$id<-as.character(vio.crime$Community.Area-1)
 
-#Population data
-pop.dat<-read.csv("C:/Users/Nick Fox/Downloads/2010_Census.csv", skip=1)
-pop.final<-data.frame(community=pop.dat$Geog,population=pop.dat$Total.Population)
-pop.final$community<-toupper(as.character(pop.final$community))
-pop.final$community[32]<-c("LOOP")
-pop.final$community[76]<-c("OHARE")
+
+
+#===========================================
+#ZIP codes for a shapefile
+#===========================================
+comm_areas<-readOGR("C:/Users/Nick Fox/Downloads/Boundaries - Community Areas/geo_export_4fbea0cb-8eca-47d2-8dd9-ba6ccb5ed7cd.shp")
+comm_areas<-readOGR("Church_Data_and_Code/Boundaries - Community Areas/geo_export_4fbea0cb-8eca-47d2-8dd9-ba6ccb5ed7cd.shp")
+
+#polygon data frame
+poly.df1 <- tidy(comm_areas,region="area_numbe")
+
+
+
+
+#===========================================
+# Final data frame for mapping
+# (add any new variables for mapping here
+# then re-run this block)
+#===========================================
+comm_data <- data.frame(id=as.character(1:77),
+                        Population=pop.dat$Total.Population,
+                        Homicide=total.homicides
+                        )
 
 #Final data sets
-poly.df1<-tidy(comm_areas,region="area_numbe")
+final.df <- left_join(poly.df1, comm_data)
+#===========================================
 
-hom.poly.df<-left_join(poly.df1,homicides)
 
-crime.pop<-left_join(area_comm,pop.final)
 
-crime_freq<-as.data.frame(table(homicides$id))
-crime_freq$id<-as.character(crime_freq$Var1)
-crime_freq.pop<-left_join(crime.pop,crime_freq)
-crime_freq.pop$rate<-crime_freq.pop$Freq/crime_freq.pop$population
 
-final.df<-left_join(hom.poly.df,crime_freq.pop)
-final.df<-combine.df[,c(1,2,6,33,34)]
 
 #Church data
 churches<-read.csv("D:/Downloads/Parish Address List.csv")
@@ -80,16 +102,71 @@ for (i in 1:dim(church_schools)[1]){
 
 church_schools_final<-cbind(church_schools,do.call(rbind,school_locations))
 
+
+
+
+#===========================================
 #Chicago Map
+#===========================================
 chimap<-get_map(location="Chicago",zoom=10,scale="auto",maptype="terrain")
 
+
+#===========================================
 #Maps the churches using raw numbers and rates
+#===========================================
+mytheme <- theme(plot.title=element_text(size=rel(1.5),face="bold"),
+                 axis.title=element_text(size=rel(1.2)),
+                 axis.text=element_text(size=rel(1.2)),
+                 #legend.title=element_blank(),
+                 legend.title=element_text(size=14,colour="black",face="bold"),
+                 legend.text=element_text(size=12,colour="black",face="bold"),
+                 # legend.background=element_rect(colour=NULL,fill="white"),
+                 legend.background=element_blank(),
+                 legend.justification=c(1,1),
+                 #legend.position=c(0.05,0.07),
+                 legend.position=c(.98,.98),
+                 legend.box="horizontal",
+                 #legend.margin=unit(.75,"cm"), 
+                 # legend.key=element_rect(colour="white"),
+                 legend.key=element_blank(),
+                 legend.key.size=unit(2,"lines"),
+                 legend.key.width=unit(.75,"cm"),
+                 legend.key.height=unit(.75,"cm"),
+                 legend.title.align=0,
+                 legend.text.align=0
+                 )
+
+
 ggmap(chimap) + 
-  geom_polygon(aes(x=long, y=lat, group=group, fill=Freq), col="black", data=final.df) +
+#ggplot() + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=Homicide), alpha=.8, col=1, size=.1, data=final.df) +
+  scale_fill_gradientn("", colors=tim.colors()) + 
+  labs(x = "Longitude", y = "Latitude") + 
+  ggtitle("Total homicides") +
+  mytheme +
+  scale_x_continuous(limits = c(-88,-87.4), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(41.6,42.1), expand = c(0, 0))
+  
+ggmap(chimap) + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=Homicide/Population), alpha=.8, col=1, size=.1, data=final.df) +
+  scale_fill_gradientn("per capita rate", colors=tim.colors()) + 
+  labs(x = "Longitude", y = "Latitude") + 
+  ggtitle("Total homicides per capita") +
+  mytheme +
+  scale_x_continuous(limits = c(-88,-87.4), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(41.6,42.1), expand = c(0, 0))
+
+
+
+
+ggmap(chimap) + 
+  geom_polygon(aes(x=long, y=lat, group=group, fill=Homicide), col="black", size=.1, data=final.df) +
   scale_fill_gradientn(colors=tim.colors()) +
   geom_point(aes(x=lon,y=lat),alpha=.7,data=churches_final) +
   scale_x_continuous(limits = c(-87.86,-87.5), expand = c(0, 0)) +
   scale_y_continuous(limits = c(41.62,42.04), expand = c(0, 0))
+
+
 
 ggmap(chimap) + 
   geom_polygon(aes(x=long, y=lat, group=group, fill=rate), data=final.df) +
