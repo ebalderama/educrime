f17a21a0db18c4a6fb2bd75d3997a2ad7c1cab2a
Nicholas Fox <Nicholas Fox>
2017-04-14 16:08:35 -0500

    Code I am using to work on the double hurdle model.

---
+++ DH Presentation Models.R
@@ -0,0 +1,25 @@
+#Modifies the data to be in numeric form, as well as fills in some missing values between columns
+CPS_2015_ES_SB[,101] <- as.numeric(as.character(CPS_2015_ES_SB[,101]))
+CPS_2015_ES_SB$SQRP_ELEM_Points_2015.y[44:47] <- c(3.8,3.8,3.8,3.8)
+CPS_2015_ES_SB$SQRP_ELEM_Points_2015.y[50:53] <- c(2.8,2.8,2.8,2.8)
+
+#This is a data set without missing values in the Y variable
+CPS_ES_Final <- CPS_2015_ES_SB[!is.na(CPS_2015_ES_SB$Expelled_N_2015),]
+
+#Creates X and Y variables for inputs
+y <- CPS_2015_ES_SB$Expelled_N_2015
+
+x <- CPS_2015_ES_SB[,c(80,82,84,86,88,90,92,94,101,347,460,480)]
+
+#Performs mean imputation for missing values in the X's
+for (i in 1:dim(x)[2]){
+  x[is.na(x[,i]),i] <- mean(x[,i], na.rm=TRUE)
+  }
+
+##Converts X to a matrix then runs the double hurdle model
+x <- as.matrix(x)
+
+dh.model<-dhurdle(Y=y,Effort=CPS_2015_ES_SB$Totals_20th_2015,X=x,
+                  lowEx = 5,V=NULL,v=NULL,spatial=c(F,F,F),
+                  iters=10000, burn=1000, keepmiss=TRUE, plot=TRUE)
+
