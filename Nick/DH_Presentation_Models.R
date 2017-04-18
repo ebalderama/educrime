26edd55be778d05cefcbbcb187c88b26b6bb514a
Nicholas Fox <Nicholas Fox>
2017-04-18 15:56:45 -0500

    Suspension model is now functional as well.

--- Nick/DH_Presentation_Models.R
+++ Nick/DH_Presentation_Models.R
@@ -19,31 +19,39 @@ y <- CPS_2015_ES_SB$Expelled_N_2015
 
 x <- CPS_2015_ES_SB[,c(80,82,88,101,347,455,476,499)]
 
-#y0 <- as.numeric(CPS_2015_ES_SB$Suspensions_ISS.OSS_N_2015)
+y0 <- as.numeric(CPS_2015_ES_SB$Suspensions_ISS.OSS_N_2015)
 
-#x0 <- CPS_2015_ES_SB[,c(80,82,86,88,90,92,101,347,455,476,499)]
+x0 <- CPS_2015_ES_SB[,c(80,82,88,101,347,455,476,499)]
 #Performs mean imputation for missing values in the X's
 for (i in 1:dim(x)[2]){
   x[is.na(x[,i]),i] <- mean(x[,i], na.rm=TRUE)
   }
 
-#for (i in 1:dim(x0)[2]){
-#  x0[is.na(x0[,i]),i] <- mean(x0[,i], na.rm=TRUE)
-#}
+for (i in 1:dim(x0)[2]){
+ x0[is.na(x0[,i]),i] <- mean(x0[,i], na.rm=TRUE)
+}
 
 ##Converts X to a matrix then runs the double hurdle model
 x <- as.matrix(scale(x))
 
-#x0 <- as.matrix(x0)
+x0 <- as.matrix(x0)
+
 school_pop <- CPS_2015_ES_SB$Totals_20th_2015
-school_pop[is.na(school_pop)] <- mean(school_pop)
+school_pop[is.na(school_pop)] <- mean(school_pop, na.rm=TRUE)
+
+school_pop0 <- CPS_2015_ES_SB$Totals_20th_2015
+school_pop0[is.na(school_pop0)] <- mean(school_pop0, na.rm=TRUE)
 
 ind_ES <- which(is.na(cbind(CPS_2015_ES_SB$Expelled_N_2015,CPS_2015_ES_SB$School_Boundary)), arr.ind=TRUE)
+ind_SUS <- which(is.na(cbind(as.numeric(CPS_2015_ES_SB$Suspensions_ISS.OSS_N_2015),CPS_2015_ES_SB$School_Boundary)), arr.ind =TRUE)
+
 #Eigenvectors
 es.evecs <- CPS_2015_ES_SB[,500:514]
-
 es.evecs <- as.matrix(es.evecs)
-#Final inputs
+
+es.evecs0 <- CPS_2015_ES_SB[,500:514]
+es.evecs0 <- as.matrix(es.evecs0)
+#Final inputs for expulsions
 y <- y[-unique(ind_ES)]
 
 x <- x[-unique(ind_ES),]
@@ -51,14 +59,23 @@ x <- x[-unique(ind_ES),]
 school_pop <- school_pop[-unique(ind_ES)]
 
 es.evecs <- es.evecs[-unique(ind_ES),]                
+#final inputs for suspensions
+y0 <- y0[-unique(ind_SUS)]
+
+x0 <- x0[-unique(ind_SUS),]
+
+school_pop0 <- school_pop0[-unique(ind_SUS)]
+
+es.evecs0 <- es.evecs0[-unique(ind_SUS),]
+
 #Elementary school expulsion models
 dh.model.ES_exp<-dhurdle(Y=y,Effort=school_pop,X=x,
                   lowEx = 1, V=es.evecs,v=v_ES,spatial=c(T,T,F),
                   iters=2000, burn=1000, nthin=10, plot=TRUE)
 
 #Elementary school suspension models
-dh.model.ES_sus<-dhurdle(Y=y0[!is.na(y0)],Effort=school_pop[!is.na(y0)],X=x0[!is.na(y0),],
-                         lowEx = 4, V=V_ES,v=v_ES,spatial=c(T,T,F),
+dh.model.ES_sus<-dhurdle(Y=y0 ,Effort=school_pop0, X=x0,
+                         lowEx = 1, V=es.evecs0,v=v_ES,spatial=c(T,T,F),
                          iters=3000, burn=500, nthin=10, plot=TRUE)
 
 for (i in 1:15){
@@ -87,48 +104,50 @@ CPS_2015_HS_SB <-left_join(CPS_2015_HS_SB,crime.count)
 
 y1 <- CPS_2015_HS_SB$Expelled_N_2015
 
-#y01 <- as.numeric(CPS_2015_HS_SB$Suspensions_ISS.OSS_N_2015)
-
 x1 <- CPS_2015_HS_SB[,c(80,82,88,101,130,499)]
 
 x1$SQRP.Total <- apply(cbind(CPS_2015_HS_SB$SQRP_HS_Points_2015.x,CPS_2015_HS_SB$SQRP_HS_Points_2015.y), 1, sum, na.rm=T)
 
 x1$SQRP.Total[x1$SQRP.Total==0] <- NA
 
-#x01 <- CPS_2015_HS_SB[,c(80,82,86,88,90,92,101,130,499)]
+y01 <- as.numeric(CPS_2015_HS_SB$Suspensions_ISS.OSS_N_2015)
 
-#x01$SQRP.Total <- apply(cbind(CPS_2015_HS_SB$SQRP_HS_Points_2015.x,CPS_2015_HS_SB$SQRP_HS_Points_2015.y), 1, sum, na.rm=T)
+x01 <- CPS_2015_HS_SB[,c(80,82,88,101,130,499)]
 
-#x01$SQRP.Total[x01$SQRP.Total==0] <- NA
+x01$SQRP.Total <- apply(cbind(CPS_2015_HS_SB$SQRP_HS_Points_2015.x,CPS_2015_HS_SB$SQRP_HS_Points_2015.y), 1, sum, na.rm=T)
+
+x01$SQRP.Total[x01$SQRP.Total==0] <- NA
+
 #Performs mean imputation
 for (i in 1:dim(x1)[2]){
   x1[is.na(x1[,i]),i] <- mean(x1[,i], na.rm=TRUE)
 }
 
-#for (i in 1:dim(x01)[2]){
- # x01[is.na(x01[,i]),i] <- mean(x01[,i], na.rm=TRUE)
-#}
+for (i in 1:dim(x01)[2]){
+  x01[is.na(x01[,i]),i] <- mean(x01[,i], na.rm=TRUE)
+}
 
 school_pop.1 <- CPS_2015_HS_SB$Totals_20th_2015
 school_pop.1[is.na(school_pop.1)] <- mean(school_pop.1, na.rm=T)
 
-#x1 <- x1[!is.na(y1),]
+school_pop.01 <- CPS_2015_HS_SB$Totals_20th_2015
+school_pop.01[is.na(school_pop.01)] <- mean(school_pop.01, na.rm=TRUE)
+
 x1 <- scale(as.matrix(x1))
 
-#x01 <- x01[!is.na(y01),]
+x01 <- scale(as.matrix(x01))
 
-#x01 <- as.matrix(x01)
-
-#x01 <- cbind(rep(1,nrow(x1)),x1)
+ind <- which(is.na(cbind(CPS_2015_HS_SB$Expelled_N_2015,CPS_2015_HS_SB$School_Boundary)), arr.ind = TRUE)
+ind01 <- which(is.na(cbind(as.numeric(CPS_2015_HS_SB$Suspensions_ISS.OSS_N_2015),CPS_2015_HS_SB$School_Boundary)),arr.ind=TRUE)
 
 #Eigenvectors
 hs.evecs <- CPS_2015_HS_SB[,500:514]
-
 hs.evecs <- as.matrix(hs.evecs)
 
-ind <- which(is.na(cbind(CPS_2015_HS_SB$Expelled_N_2015,CPS_2015_HS_SB$School_Boundary)), arr.ind = TRUE)
+hs.evecs01 <- CPS_2015_HS_SB[,500:514]
+hs.evecs01 <- as.matrix(hs.evecs01)
 
-#Final inputs
+#Final inputs for expulsion model
 y1 <- y1[-unique(ind)]
 
 x1 <- x1[-unique(ind),]
@@ -136,6 +155,14 @@ x1 <- x1[-unique(ind),]
 school_pop.1 <- school_pop.1[-unique(ind)]
 
 hs.evecs <- hs.evecs[-unique(ind),]
+#final inputs for suspension model
+y01 <- y01[-unique(ind01)]
+
+x01 <- x01[-unique(ind01),]
+
+school_pop.01 <- school_pop.01[-unique(ind01)]
+
+hs.evecs01 <- hs.evecs01[-unique(ind01),]
 
 #High School Expulsions
 dh.model.HS_exp <- dhurdle(Y=y1, Effort=school_pop.1, X=x1,
@@ -144,9 +171,9 @@ dh.model.HS_exp <- dhurdle(Y=y1, Effort=school_pop.1, X=x1,
 
 
 #High School Suspensions
-dh.model.HS_sus <- dhurdle(Y=y01[!is.na(y01)],Effort=school_pop.1[!is.na(y01)],X=x01,
-                           lowEx = 83, V=V_HS, v=v_HS, spatial=c(T,T,F),
-                           iters=3000, burn=500, nthin=10, plot=TRUE)
+dh.model.HS_sus <- dhurdle(Y=y01,Effort=school_pop.01,X=x01,
+                           lowEx = 1, V=hs.evecs01, v=v_HS, spatial=c(T,T,F),
+                           iters=2000, burn=1000, nthin=10, plot=TRUE)
 
 
 z.means1 <- vector()
